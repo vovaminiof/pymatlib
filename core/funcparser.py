14ed753895785a2ad1e09bbad4e890bed7550f10
unknown <vminof@Delhi.softservecom.com>
2013-07-29 14:15:56 +0300

    added intargal module

---
+++ core/funcparser.py
@@ -0,0 +1,37 @@
+# funcparser.py
+import re
+import imp
+import math
+
+class Parser(object):
+
+    def __init__(self, function):
+        def translate(f):
+            return re.sub('(x)[^^]|&', 'x^1', f)
+
+        def power(f):
+            return re.sub('x\^(\d+)', lambda g: 'pow(x,%s)' % g.group(1), f)
+
+        f = translate(function)
+        self._max_power = self.__get_max_power(f)
+        f = power(f)
+        self._function = self.__parse_str(f)
+
+    def __get_max_power(self, f):
+        powers = re.findall('\^(\d)', f)
+        return int(max(powers if powers else [0]))
+
+    def __parse_str(self, f):
+        ''' Parsing string function into lambda '''
+        code = "def f(x): return " + f
+        module = imp.new_module('calculus')
+        exec code in math.__dict__, module.__dict__
+        return module.f
+
+    @property
+    def func(self):
+        return self._function
+
+    @property
+    def power(self):
+        return self._max_power
---
+++ core/intergals.py
@@ -0,0 +1,52 @@
+# intergals.py
+from funcparser import Parser
+
+class Intergal(object):
+    def __init__(self, function, a, b):
+        parser = Parser(function)
+        self.f = parser.func
+        self.max_power = parser.power
+        self.a = a
+        self.b = b
+        self._value = None
+        self._calc()
+
+    def _calc(self):
+        self.n = 100000
+        if self.max_power == 0:
+            self._value = self.__rectangle_rule()
+        if self.max_power == 1:
+            self._value = self.__trapezoidal_rule()
+        elif self.max_power >= 2:
+            self._value = self.__simpsons_rule()
+
+    def __rectangle_rule(self):
+        h = float((self.b - self.a)) / self.n
+        result = 0.0
+        for i in range(1, self.n + 1):
+            result += self.f(self.a + (2*i - 1)*h/2.0)
+
+        return h*result
+
+    def __trapezoidal_rule(self):
+        h = float((self.b - self.a)) / (self.n)
+        result = self.f(self.a) + self.f(self.b)
+        for i in range(2, self.n):
+            result += 2 * self.f(self.a + (i-1)*h)
+
+        return h*result / 2.0
+
+    def __simpsons_rule(self):
+        m = self.n / 2
+        h = float((self.b - self.a))/self.n
+        result = self.f(self.a)
+        for i in range(1, self.n):
+            index = 4 if i % 2 != 0 else 2
+            result += index * self.f(self.a + i*h)
+
+        result += self.f(self.a + self.n*h)
+        return (h/3.0)*result
+
+    @property
+    def value(self):
+        return self._value
